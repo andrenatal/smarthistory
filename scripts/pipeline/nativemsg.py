db412e9d9bc167608b5001f89622832d91d97c0f
Andre Natal <andrenatal@users.noreply.github.com>
2022-11-17 00:01:04 -0600

    WIP

---
+++ scripts/pipeline/nativemsg.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""Interactive interface to full DrQA pipeline."""
+
+import torch
+import argparse
+import prettytable
+import logging
+import sys
+import json
+import struct
+
+from termcolor import colored
+from drqa import pipeline
+from drqa.retriever import utils
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--reader-model', type=str, default=None,
+                    help='Path to trained Document Reader model')
+parser.add_argument('--retriever-model', type=str, default=None,
+                    help='Path to Document Retriever model (tfidf)')
+parser.add_argument('--doc-db', type=str, default=None,
+                    help='Path to Document DB')
+parser.add_argument('--tokenizer', type=str, default=None,
+                    help=("String option specifying tokenizer type to "
+                          "use (e.g. 'corenlp')"))
+parser.add_argument('--candidate-file', type=str, default=None,
+                    help=("List of candidates to restrict predictions to, "
+                          "one candidate per line"))
+parser.add_argument('--no-cuda', action='store_true',
+                    help="Use CPU only")
+parser.add_argument('--gpu', type=int, default=-1,
+                    help="Specify GPU device id to use")
+args = parser.parse_args()
+
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+if args.cuda:
+    torch.cuda.set_device(args.gpu)
+    logger.info('CUDA enabled (GPU %d)' % args.gpu)
+else:
+    logger.info('Running on CPU only.')
+
+if args.candidate_file:
+    logger.info('Loading candidates from %s' % args.candidate_file)
+    candidates = set()
+    with open(args.candidate_file) as f:
+        for line in f:
+            line = utils.normalize(line.strip()).lower()
+            candidates.add(line)
+    logger.info('Loaded %d candidates.' % len(candidates))
+else:
+    candidates = None
+
+logger.info('Initializing pipeline...')
+DrQA = pipeline.DrQA(
+    cuda=args.cuda,
+    fixed_candidates=candidates,
+    reader_model=args.reader_model,
+    ranker_config={'options': {'tfidf_path': args.retriever_model}},
+    db_config={'options': {'db_path': args.doc_db}},
+    tokenizer=args.tokenizer
+)
+logger.info('Ready')
+
+
+# ------------------------------------------------------------------------------
+# Drop in to interactive mode
+# ------------------------------------------------------------------------------
+
+
+def process(question, candidates=None, top_n=1, n_docs=5):
+    predictions = DrQA.process(
+        question, candidates, top_n, n_docs, return_context=True
+    )
+    table = prettytable.PrettyTable(
+        ['Rank', 'Answer', 'Doc', 'Answer Score', 'Doc Score']
+    )
+#    for i, p in enumerate(predictions, 1):
+#        logger.info([i, p['span'], p['doc_id'],
+#                       '%.5g' % p['span_score'],
+#                       '%.5g' % p['doc_score']])
+#    logger.info('Top Predictions:')
+#    logger.info(table)
+#    logger.info('\nContexts:')
+    for p in predictions:
+        text = p['context']['text']
+        start = p['context']['start']
+        end = p['context']['end']
+        output = (text[:start] +
+                  colored(text[start: end], 'green', attrs=['bold']) +
+                  text[end:])
+        logger.info('[ Doc = %s ]' % p['doc_id'])
+        logger.info(output + '\n')
+
+
+banner = """
+Interactive DrQA
+>> process(question, candidates=None, top_n=1, n_docs=5)
+>> usage()
+"""
+
+
+# Python 3.x version
+# Read a message from stdin and decode it.
+def getMessage():
+    rawLength = sys.stdin.buffer.read(4)
+    if len(rawLength) == 0:
+        sys.exit(0)
+    messageLength = struct.unpack('@I', rawLength)[0]
+    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+    return json.loads(message)
+
+# Encode a message for transmission,
+# given its content.
+def encodeMessage(messageContent):
+    encodedContent = json.dumps(messageContent).encode('utf-8')
+    encodedLength = struct.pack('@I', len(encodedContent))
+    return {'length': encodedLength, 'content': encodedContent}
+
+# Send an encoded message to stdout
+def sendMessage(encodedMessage):
+    sys.stdout.buffer.write(encodedMessage['length'])
+    sys.stdout.buffer.write(encodedMessage['content'])
+    sys.stdout.buffer.flush()
+
+while True:
+    #try:
+    #    process("What is the element used to create a hyperlink to webpages?", top_n=5, n_docs=5)
+    #except:
+    #    sendMessage(encodeMessage("error process"))
+    #sendMessage(encodeMessage("ready"))
+    receivedMessage = getMessage()
+    if receivedMessage == "ping":
+        sendMessage(encodeMessage("pong"))
+        process("What is the element used to create a hyperlink to webpages?", top_n=5, n_docs=5)
