2702595d8af81d6a617fca3332cb3b3e9ed24df0
Andre Natal <anatal@gmail.com>
2023-06-29 11:50:41 -0700

    Adding filter by confidence

--- add-on/sidebar_panel.js
+++ add-on/sidebar_panel.js
@@ -32,30 +32,39 @@ browser.runtime.onMessage.addListener((message) => {
         document.getElementById("status").innerText = "";
         const divResposta = document.createElement("div");
         divResposta.className = "divResposta";
-
+        const confidenceFilter = document.getElementById("confidenceFilter").value;
         for (let response in message.responses) {
-            const answer = document.createElement("a");
-            answer.href = message.responses[response].hitpayload.url;
-            answer.innerText = String.fromCodePoint(0x1F64B) + " " + message.responses[response].answer.answer.replace(/(\r\n|\n|\r)/gm, "");
-            answer.className = "respostaLink";
+            if (document.getElementById("filterByconfidence").checked &&
+                (message.responses[response].hitscore < confidenceFilter || message.responses[response].answer.score < confidenceFilter)) {
+                    const confidence = document.createElement("a");
+                    confidence.innerText = "This response was filter due low confidence.";
+                    confidence.className = "respostaLink";
+                    confidence.title = message.responses[response].hitscore  + "," + message.responses[response].answer.score;
+                    divResposta.append(document.createElement("br"));
+                    divResposta.append(confidence);
+            } else {
+                const answer = document.createElement("a");
+                answer.href = message.responses[response].hitpayload.url;
+                answer.innerText = String.fromCodePoint(0x1F64B) + " " + message.responses[response].answer.answer.replace(/(\r\n|\n|\r)/gm, "");
+                answer.className = "respostaLink";
 
-            const href = document.createElement("a");
-            href.href = message.responses[response].hitpayload.url;
-            href.innerText = String.fromCodePoint(0x1F517) + " " + message.responses[response].hitpayload.title;
-            href.className = "respostaLink";
+                const href = document.createElement("a");
+                href.href = message.responses[response].hitpayload.url;
+                href.innerText = String.fromCodePoint(0x1F517) + " " + message.responses[response].hitpayload.title;
+                href.className = "respostaLink";
 
-            divResposta.append(answer);
-            divResposta.append(document.createElement("br"));
-            divResposta.append(href);
-            if (message.responses[response].hitscore < 0.4 || message.responses[response].answer.score < 0.4) {
-                const confidence = document.createElement("a");
-                confidence.innerText = "Low confidence";
-                confidence.className = "confidence";
-                confidence.title = message.responses[response].hitscore  + "," + message.responses[response].answer.score;
+                divResposta.append(answer);
                 divResposta.append(document.createElement("br"));
-                divResposta.append(confidence);
+                divResposta.append(href);
+                if ((message.responses[response].hitscore < confidenceFilter || message.responses[response].answer.score < confidenceFilter)) {
+                    const confidence = document.createElement("a");
+                    confidence.innerText = "Low confidence";
+                    confidence.className = "confidence";
+                    confidence.title = message.responses[response].hitscore  + "," + message.responses[response].answer.score;
+                    divResposta.append(document.createElement("br"));
+                    divResposta.append(confidence);
+                }
             }
-            //divResposta.innerHTML = message.hitscore + "<br>" + message.hitpayload.url + "<br>" + message.answer.answer + "<br>" + message.hitpayload.title;
             document.getElementById("respostas").append(divResposta);
             divResposta.append(document.createElement("hr"));
         }
